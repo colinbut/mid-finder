b2feac90e987433100be0708ab88c663d9900ba7
Colin But <colinbut@users.noreply.github.com>
2018-02-28 22:23:09 +0000

    making two test case pass

---
+++ src/main/java/com/mycompany/midfinder/MidFinder.java
@@ -0,0 +1,20 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2018 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.midfinder;
+
+public class MidFinder {
+    public int findMidIndex(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException();
+        }
+
+        if (input == "") {
+            throw new IllegalArgumentException();
+        }
+
+        return 0;
+    }
+}
